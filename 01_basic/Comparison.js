5018151b459c9a5192b7b8fd37fd2e08857848b0
kadirmalik787 <110757075+kadirmalik787@users.noreply.github.com>
2024-04-29 13:18:26 +0000

    summary

---
+++ 01_basic/Comparison.js
@@ -0,0 +1,8 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2!=1); 2 not equal 1 is true
+
+// console.log(null>0); false 
+// console.log(null==0); false 
+// console.log(null>=0); true
---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,31 @@
+// kis trh se data ko memory me rkha jat hai us trh se data ke 2 type haai
+// 1=== primitive data type 
+// its 7 type of primitive data type
+// 1 String
+// 2 Number
+// 3 Boolearn
+// 4 undefined
+// 5 bigInt
+// 6 null
+// 7 Symbol
+
+// ye sb call by value hote hai inko copy krke alg diya jata hai 
+
+// Reference type(non primitive)
+
+// 1 Array
+// 2 object agr js master krni hai to object master krlo or browser Event master krlo bs 
+// 3 function
+
+
+// javascript is dynamically typed language
+
+// =============================memory ==================
+// ye bhi 2 type ki hoti hai
+
+// stack(primitive type me stack use hoti hai)  and heap(non-primitive me use hoti hai)
+
+// stack or primitive type me jo bhi variable declare kiya hai uski copy milti hai
+//heap non-primitive me isme original value ka reference milta hai 
+
+// ex===
