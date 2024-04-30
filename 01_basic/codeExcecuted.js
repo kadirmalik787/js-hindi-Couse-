ac2be4075b4858a6f7a1b9a1771bd950918b45d2
kadirmalik787 <110757075+kadirmalik787@users.noreply.github.com>
2024-04-30 19:07:22 +0000

    execution

---
+++ 01_basic/codeExcecuted.js
@@ -0,0 +1,24 @@
+console.log(fristName)
+
+var fristName = "kadir"
+let LastName = "malik"
+let age= 23
+console.log(fristName)
+
+const yearOfBirth = 2000
+ console.log(age)
+let useIntro = "hi my name is"  + fristName +' '+LastName
+
+// code excecute in javascript
+//first time me memory creation hota hai javascript phle bs ye dekhega ki yha var hai let hai or const hai bs ye chize dekhta hai
+
+// ye ye first line se firstName ko lega or memoy me rkh lega or usko value asign nhi krega usko undefined rkhega bs or iska typeOf bhi undefined ho jayega
+// same second line me hoga usko bhi undefined kr dega usko or uska typeOf bhi undefined rhega 
+// chro line me undefined ho jyega or unke typeOf ke hisb se unko data de diya jyega first time  me js ka interpreter yhi kam krta hai
+//is pure face ko bolte hai memory creation phase
+//====== yha 6 number line ko chor dega yha is phase me bs ye let const var dekh rha hau is liye ye sb chor dega ==ye pura memory creation phase hai
+
+// ab yha se code execution phase =======
+//ab fristName wle variable ko value asign ho jyega or aa jyega undefined ki jgh kadir or typeOf bhi change ho jyega or ho jyega string
+//vaise bhi sbme aa jyega or unke hisb se unko data type mil jyega 
+// ==or sb execution phase hota hai to wo ak ak line ko dekhta hai ex hmne sbse uper hi console.log(age) ye likh diya qki ye to phle undefined tha or ye ab first line pr aa gya execute hone to ye abhi asign hua nhi hai to ye ab bhi undefined ayega
